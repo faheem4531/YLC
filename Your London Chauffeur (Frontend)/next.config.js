011c638a74125f77517d8bb15f6abecd81328426
faheem4531 <aa0561085@gmail.com>
2023-08-25 00:25:09 +0500

    packages installation (yarn install)

--- Your London Chauffeur (Frontend)/next.config.js
+++ Your London Chauffeur (Frontend)/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ylc-media.s3.eu-west-2.amazonaws.com', 'ylc-media.s3.amazonaws.com'],
+    domains: ['ylc-media.s3.eu-west-2.amazonaws.com', 'ylc-media.s3.amazonaws.com', 'res.cloudinary.com'],
   },
 }
 
